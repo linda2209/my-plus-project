d558db6d48ed24c374c0eeaad5c68c73f11bb37a
linda2209 <85490327+linda2209@users.noreply.github.com>
2021-07-31 23:19:32 +0700

    change the quote

--- src/script.js
+++ src/script.js
@@ -153,3 +153,7 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
+if (description.innerHTML === "broken clouds") {
+  alert("hello");
+}
